493fae551cfd970b1abb406589408e2808e48d7e
abdelrahmanamin410-cyber <abdelrahmanamin410@gmail.com>
2025-11-02 01:14:48 +0200

    ggggg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,8 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1580556331698002"
           crossOrigin="anonymous"
         />
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="YOUR_VERIFICATION_CODE" />
       </Head>
       <body>
         <Main />
